b6ac8797d49e7416280452618a6a89f576adbf28
elcinbalci <elcin.balci@heyteknoloji.com>
2026-07-07 15:29:18 +0300

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/balyalama-mock-up/',
+  base: '/balyalama-mock-up-v2/',
   plugins: [react()],
 })
